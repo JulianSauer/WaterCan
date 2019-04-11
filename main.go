88c2a6348d8f94dc1e1e6ceafe10682394babb3a
Julian Sauer <julian_sauer@gmx.net>
2019-04-11 18:08:39 +0200

    Timeout for update routines

--- main.go
+++ main.go
@@ -54,7 +54,7 @@ func UpdateMoisture(context echo.Context) error {
     }
 
     rgb := plant_state.Parse(moistureLevel)
-    go updateLight(light, rgb[0], rgb[1], rgb[2], context.Logger())
+    go updateLight(light, rgb[0], rgb[1], rgb[2], -1, context.Logger())
 
     context.Logger().Print("Current moisture is ", moistureLevel, "%")
 
@@ -66,6 +66,8 @@ func UpdateMoisture(context echo.Context) error {
 //          If not provided, the default sensors will be loaded from config.json
 // light:   Use the specific id of a Philiphs Hue light
 //          If not provided, the default light will be loaded from config.json
+// timeout: Timeout in seconds
+//          If non provided, it will try forever to change the color
 func ForceUpdate(context echo.Context) error {
     ids, e := getSensorFromParam(context)
     if e != nil {
@@ -82,8 +84,13 @@ func ForceUpdate(context echo.Context) error {
         return context.String(logError(context.Logger(), e))
     }
 
+    timeout, e := getTimeoutFromParam(context)
+    if e != nil {
+        return context.String(logError(context.Logger(), e))
+    }
+
     rgb := plant_state.Parse(moistureLevel)
-    go updateLight(light, rgb[0], rgb[1], rgb[2], context.Logger())
+    go updateLight(light, rgb[0], rgb[1], rgb[2], timeout, context.Logger())
 
     return context.String(http.StatusOK, "")
 }
@@ -93,9 +100,16 @@ func logError(logger echo.Logger, e error) (int, string) {
     return http.StatusBadRequest, e.Error()
 }
 
-func updateLight(id int, red float64, green float64, blue float64, logger echo.Logger) {
-    logger.Print("Will update light with id ", id, " to color (", red, ", ", green, ", ", blue, ") as soon as it's turned on")
-    for {
+func updateLight(id int, red float64, green float64, blue float64, timeout int, logger echo.Logger) {
+    timeoutDisabled := false
+    if timeout >= 0 {
+        logger.Print("Will update light with id ", id, " to color (", red, ", ", green, ", ", blue, ") if it's turned on within the next ", timeout, " seconds")
+        timeout = timeout / 10
+    } else {
+        logger.Print("Will update light with id ", id, " to color (", red, ", ", green, ", ", blue, ") as soon as it's turned on")
+        timeoutDisabled = true
+    }
+    for ; timeoutDisabled || timeout >= 0; timeout-- {
         e := hue.SetLightColor(id, red, green, blue)
         if e != nil {
             if e.Error() == "parameter, xy, is not modifiable. Device is set to off." {
@@ -105,9 +119,9 @@ func updateLight(id int, red float64, green float64, blue float64, logger echo.L
                 logger.Print(e.Error())
             }
         }
+        logger.Print("Updated light with id ", id, " to color (", red, ", ", green, ", ", blue, ")")
         break
     }
-    logger.Print("Updated light with id ", id, " to color (", red, ", ", green, ", ", blue, ")")
 }
 
 func moistureOfDriestPlant(ids []int) (float64, error) {
@@ -168,5 +182,13 @@ func getSensorFromParam(context echo.Context) ([]int, error) {
             return []int{}, e
         }
         return []int{id}, nil
+    }
+}
+
+func getTimeoutFromParam(context echo.Context) (int, error) {
+    if context.QueryParam("timeout") == "" {
+        return -1, nil
+    } else {
+        return strconv.Atoi(context.QueryParam("timeout"))
     }
 }
